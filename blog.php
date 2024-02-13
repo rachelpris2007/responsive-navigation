2a977c80843659cef1969ca567a6618cdc7250fb
rachelpris2007 <rachelpriskila144@gmail.com>
2024-02-13 13:01:48 +0700

    navigation

---
+++ blog.php
@@ -0,0 +1,7 @@
+<div id="content-product">
+    <h1>Title Product</h1><br>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolore, quod rem numquam sapiente blanditiis optio
+       voluptate accusamus autem corporis magnam cum assumenda, dolor ab unde incidunt distinctio aperiam totam.
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolore, quod rem numquam sapiente blanditiis optio
+       voluptate accusamus autem corporis magnam cum assumenda, dolor ab unde incidunt distinctio aperiam totam.</p>
+</div>
